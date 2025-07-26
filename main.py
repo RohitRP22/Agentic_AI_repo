94d01933737e3fcd2d92a58d630ac71333155048
RohitRP22 <rohitpidurkar@gmail.com>
2025-07-26 23:04:55 +0530

    FastApi interface

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+from typing import List, Dict
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
 def main():
-    print("Hello from agenti-ai!")
+    print("Hello from agentic-ai!")
 
 
 if __name__ == "__main__":
